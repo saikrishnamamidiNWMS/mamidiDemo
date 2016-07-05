6899811d04d363e9d9cc4dd0690d738232ac95e6
S525053 <S525053@NW32489.nwmissouri.edu>
2016-07-05 15:49:12 -0500

    Created MamidiCalculator Project & finished writing logic for 2 methods - add, sub in Calculator.java class

---
+++ src/mamidicalculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mamidicalculator;
+
+/**
+ *
+ * @author S525053
+ */
+public class Calculator {
+private int a;
+private int b;
+
+public int add(){
+    return a+b;
+}
+        
+public int sub(){
+    return a-b;
+}    
+}
